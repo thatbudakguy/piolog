cb2a4ec038993a40693e478b6809bc56699f49f2
thatbudakguy <thatbudakguy@gmail.com>
2015-11-16 15:46:11 -0800

    Removed slider subtext and footer garbage on mobile

---
+++ footer.php
@@ -0,0 +1,11 @@
+</div>
+<footer class="mh-footer">
+	<div class="wrapper-inner">
+		<p class="copyright"><?php echo 'Copyright 2015 | Lewis & Clark Pioneer Log' ?></p>
+	</div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
+
+<!-- 'mh-newsdesk-lite'), date("Y"), '<a href="' . esc_url('http://www.mhthemes.com/') . '" rel="nofollow">MH Themes</a>' -->
